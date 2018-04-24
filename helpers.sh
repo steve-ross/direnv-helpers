568a5c7fd7f5c08f0effc60aeb26d83b1d0ee3a8
Steve Ross <steve@oak.works>
2018-04-24 14:17:58 -0400

    🎨 Added colorful logging

--- helpers.sh
+++ helpers.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 __prompt_install_nvm(){
-  log_status "Couldn't find nvm (node version manager)..."
+  _log warn "Couldn't find nvm (node version manager)..."
   read -p "Should I install it? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    log_status "Installing NVM"
+    _log info "Installing NVM"
     curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
     __source_nvm # make sure nvm is sourced
   else
     log_error "Install nvm first and make sure it is in your path and try again"
-    log_status "To install NVM visit https://github.com/creationix/nvm#installation"
+    _log warn "To install NVM visit https://github.com/creationix/nvm#installation"
     exit
   fi
 }
 
 __prompt_install_meteor(){
-  log_status "Couldn't find meteor..."
+  _log info "Couldn't find meteor..."
   read -p "Should I install it? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    log_status "Installing, this will take awhile."
+    _log info "Installing, this will take awhile."
     curl https://install.meteor.com/ | sh
   else
     log_error "Install meteor and try again"
-    log_status "To install NVM visit https://www.meteor.com/install"
+    _log warn "To install NVM visit https://www.meteor.com/install"
     exit
   fi
 }
@@ -86,10 +86,44 @@ __nvm_use_or_install_version(){
   local version=$(< .nvmrc)
   local nvmrc_node_version=$(nvm version "$version")
   if [ "$nvmrc_node_version" = "N/A" ]; then
-    log_status "Installing missing node version"
+    _log warn "Installing missing node version"
     local install_output=$(nvm install "$version")
   fi
   nvm use
+}
+
+_log() {
+  local msg=$*
+  local color_normal
+  local color_success
+  
+  color_normal=$(tput sgr0)
+  color_success=$(tput setaf 2)
+  color_warn=$(tput setaf 3)
+  color_info=$(tput setaf 5)
+
+  # default color
+  current_color="${color_normal}"
+
+  if ! [[ -z $2 ]]; then
+    local message_type=$1
+    # remove message type from the string (plus a space)
+    msg=${msg/$message_type /}
+    if [ "$message_type" = "warn" ]; then
+      current_color="${color_warn}"
+    fi
+    if [ "$message_type" = "info" ]; then
+      current_color="${color_info}"
+    fi
+    if [ "$message_type" = "success" ]; then
+      current_color="${color_success}"
+    fi
+  fi
+
+  if [[ -n $DIRENV_LOG_FORMAT ]]; then
+    # shellcheck disable=SC2059
+    printf "${current_color}${DIRENV_LOG_FORMAT}${color_normal}\n" "$msg" >&2
+  fi
 }
 
 requires_nvm(){
@@ -104,7 +138,7 @@ __config_or_init_stencil(){
   if [ -z "$STENCIL_CONFIG" ]; then
     stencil init
   else
-    log_status "Good to go, 'stencil start' for local development"
+    _log success "Good to go, 'stencil start' for local development"
   fi
 }
 
@@ -112,7 +146,7 @@ requires_stencil(){
   if has stencil; then
     __config_or_init_stencil
   else
-    log_status "Installing stencil cli"
+    _log warn "Installing stencil cli"
     npm install -g @bigcommerce/stencil-cli
   fi
   
@@ -120,9 +154,9 @@ requires_stencil(){
 
 requires_themekit(){
   if has theme; then
-    log_status "Found shopify themekit"
+    _log success "Found shopify themekit"
   else
-    log_status "Installing shopify themekit"
+    _log warn "Installing shopify themekit"
     # mac only here, need to detect this instead
     brew tap shopify/shopify
     brew install themekit
@@ -133,11 +167,11 @@ requires_meteor(){
   if has meteor; then
     if [ ! -d ./node_modules ]; then
       # no node modules... run meteor npm install
-      log_status "Running npm install"
+      _log warn "Running npm install"
       meteor npm install
     fi
   else
     __prompt_install_meteor
   fi
-  log_status "Good to go, Meteor installed"
+  _log success "Good to go, Meteor installed"
 }
